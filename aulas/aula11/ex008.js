f18f444d4036d49ade8c96e3f08008d9f6e68b21
Rafael Alves <FaelAlvezzz@Gmail.com>
2024-05-31 23:14:11 -0300

    Refaturei os exercícios e criei novos exercícios.

---
+++ aulas/aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 78
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if (vel > 60) { // CONDIÇÃO SIMPLES!!!
+    console.log(`Você ultrapassou o limete permitido. MULTADO!!`)
+}
+console.log(`Sempre que dirigir use cinto de segurança!`)
