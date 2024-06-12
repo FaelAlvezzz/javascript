e7139e1e0468d18b60d912c75964e57c98e6f420
Rafael Alves <FaelAlvezzz@Gmail.com>
2024-06-12 09:31:15 -0300

    Aula 15

---
+++ aulas/aula15/ambiente.js
@@ -0,0 +1,6 @@
+let num = [5, 8, 3, 7, 2]
+num.push(1) // Ele adiciona um valor no ultimo índice
+num.sort() // Ele organiza os valores em ordem decrescente
+console.log(num)
+console.log(`O vetor tem ${num.length} posições`) // Ele mostra a quantidade de elemento
+console.log(`O primeiro valor é o ${num[0]}`)
