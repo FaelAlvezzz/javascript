73d5cac92c01eb450779953d1201bc2acccc9975
Rafael Alves <FaelAlvezzz@Gmail.com>
2024-06-14 09:35:54 -0300

    Aula 15 feita

--- aulas/aula15/ambiente.js
+++ aulas/aula15/ambiente.js
@@ -4,3 +4,9 @@ num.sort() // Ele organiza os valores em ordem decrescente
 console.log(num)
 console.log(`O vetor tem ${num.length} posições`) // Ele mostra a quantidade de elemento
 console.log(`O primeiro valor é o ${num[0]}`)
+let pos = num.indexOf(4) // Esse indexOf() encontra a posição valor do elemento.
+if (pos== -1) {
+    console.log('O valor não foi encontrado!')
+}else {
+    console.log(`O valor 8 está na posição ${pos}`)
+}
